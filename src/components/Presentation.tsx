59c04806a739b933008e11cc7a2fb57e01cc7362
MatiasJRB <40368281+MatiasJRB@users.noreply.github.com>
2025-04-09 15:24:07 -0300

    Update resume summary and enhance Presentation component

--- src/components/Presentation.tsx
+++ src/components/Presentation.tsx
@@ -6,6 +6,7 @@ interface Basics {
   name: string;
   label: string;
   summary: string;
+  email: string;
 }
 
 const Presentation: React.FC = () => {
@@ -23,6 +24,11 @@ const Presentation: React.FC = () => {
       </div>
       <p className="mt-2 lg:mt-8 text-base leading-relaxe max-w-xs md:max-w-sm lg:max-w-sm">
         {basics.summary}
+      </p>
+
+      <p className="mt-2 lg:mt-8 text-base leading-relaxed max-w-xs md:max-w-sm lg:max-w-sm">
+        I am always looking for new opportunities to work on exciting projects.
+        If you have an idea or a project in mind, feel free to reach me out.
       </p>
     </div>
   );
