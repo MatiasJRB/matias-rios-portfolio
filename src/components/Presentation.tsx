08f6d11f2d447a8f6a14d0caeb02847f2cacf884
MatiasJRB <40368281+MatiasJRB@users.noreply.github.com>
2025-04-12 18:04:13 -0300

    Enhance Presentation component layout and add Makefile for development setup

--- src/components/Presentation.tsx
+++ src/components/Presentation.tsx
@@ -22,11 +22,11 @@ const Presentation: React.FC = () => {
       <div className="mt-2 text-lg lg:text-xl text-stone-200">
         {basics.label}
       </div>
-      <p className="mt-2 lg:mt-8 text-base leading-relaxe max-w-xs md:max-w-sm lg:max-w-sm">
+      <p className="mt-4 lg:mt-8 text-base leading-relaxe max-w-xs md:max-w-sm lg:max-w-sm">
         {basics.summary}
       </p>
 
-      <p className="mt-2 lg:mt-8 text-base leading-relaxed max-w-xs md:max-w-sm lg:max-w-sm">
+      <p className="mt-4 lg:mt-8 text-base leading-relaxed max-w-xs md:max-w-sm lg:max-w-sm">
         I am always looking for new opportunities to work on exciting projects.
         If you have an idea or a project in mind, feel free to reach me out.
       </p>
